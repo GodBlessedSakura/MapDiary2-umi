4ef3bcb3777a362aacd75cdad26e1900952d5c46
Zhang Lin <1306562902@qq.com>
2023-02-25 10:23:34 +0800

    fix: 单独对favicon设置path

--- ui/config/config.js
+++ ui/config/config.js
@@ -30,4 +30,5 @@ export default defineConfig({
   },
   base: '/map', // 部署HTML到非根目录
   publicPath: 'http://bluerosefantasy.com/map/',
+  favicon: 'http://bluerosefantasy.com/map/favicon.ico', // publicPath只对css, js生效，对 favicon 不生效
 })
